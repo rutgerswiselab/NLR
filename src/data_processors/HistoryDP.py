fc291d74bc6865625972e2742dc8f953deccb45e
The WISE Lab @ Rutgers CS <rutgerswiselab@gmail.com>
2020-12-19 12:49:45 -0500

    Update HistoryDP.py

--- src/data_processors/HistoryDP.py
+++ src/data_processors/HistoryDP.py
@@ -13,6 +13,7 @@ from utils.global_p import *
 
 class HistoryDP(DataProcessor):
     # data dict中存储模型所需特征信息的key，需要转换为tensor
+    # The key to store the feature information needed by the model in data dict
     data_columns = [UID, IID, X, C_HISTORY, C_HISTORY_NEG]
     info_columns = [SAMPLE_ID, TIME, C_HISTORY_LENGTH, C_HISTORY_NEG_LENGTH]  # data dict中存储模额外信息的key
 
@@ -20,6 +21,10 @@ class HistoryDP(DataProcessor):
     def parse_dp_args(parser):
         """
         数据处理生成batch的命令行参数
+        :param parser:
+        :return:
+        
+        Command-line parameters to generate batches in data processing
         :param parser:
         :return:
         """
@@ -57,6 +62,15 @@ class HistoryDP(DataProcessor):
         :param neg_data: 负例的data dict，如果已经有可以传入拿来用
         :param special_cols: 需要特殊处理的column
         :return: batch的feed dict
+        
+        topn model will produce a batch, if doing training then need to sample a negative example for each positive example, and garanttee that for each batch the first half are positive examples and the second half are negative examples
+        :param data: data dict, produced by self.get_*_data() and self.format_data_dict() functions
+        :param batch_start: starting index of the batch
+        :param batch_size: batch size
+        :param train: training or testing
+        :param neg_data: data dict of negative examples, if alreay exist can use directly
+        :param special_cols: columns that need special treatment
+        :return: feed dict of the batch
         """
         feed_dict = DataProcessor.get_feed_dict(
             self, data, batch_start, batch_size, train, neg_data=neg_data,
@@ -64,13 +78,17 @@ class HistoryDP(DataProcessor):
             if special_cols is None else [C_HISTORY, C_HISTORY_NEG] + special_cols)
 
         his_cs, his_ls = [C_HISTORY], [C_HISTORY_LENGTH]
-        if C_HISTORY_NEG in feed_dict:  # 如果有负向历史的列
+        # 如果有负向历史的列
+        # If there are columns of negative histories
+        if C_HISTORY_NEG in feed_dict:
             his_cs.append(C_HISTORY_NEG)
             his_ls.append(C_HISTORY_NEG_LENGTH)
 
         for i, c in enumerate(his_cs):
             lc, d = his_ls[i], feed_dict[c]
-            if self.sparse_his == 1:  # 如果是稀疏表示
+            # 如果是稀疏表示
+            # If it's sparse representation
+            if self.sparse_his == 1:
                 x, y, v = [], [], []
                 for idx, iids in enumerate(d):
                     x.extend([idx] * len(iids))
@@ -106,11 +124,18 @@ class HistoryDP(DataProcessor):
         :param df: 训练、验证、测试df
         :param model: Model类
         :return:
+        
+        Except for normal uid,iid,label,user, item, context features, also need to process history interactions
+        :param df: training, validation, and testing df
+        :param model: Model class
+        :return:
         """
-        assert C_HISTORY in df  # history需要data_loader放在df里
+        # history需要data_loader放在df里
+        # history need to be put into df by data_loader
+        assert C_HISTORY in df
 
         his_cs = [C_HISTORY]
-        if C_HISTORY_NEG in df:  # 如果有负向历史的列
+        if C_HISTORY_NEG in df:  # 如果有负向历史的列 (if there exist columns of negative histories)
             his_cs.append(C_HISTORY_NEG)
         if self.drop_first == 1:
             for c in his_cs:
